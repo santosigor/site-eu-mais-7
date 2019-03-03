4f4dd1f0cf5b9f9766f8ac39650c09d9bb235218
Igor Santos <igor.santos@vilt-group.com>
2019-03-03 09:25:02 -0300

    dev

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function (){
 
+	$(window).scroll(function() {
+    bgNav();
+	});
 
+	bgNav();
 
 });
+
+function bgNav() {
+ if ($(this).scrollTop()>84) {
+    $('.ems-header__nav').css('background','#020202');
+  } else {
+    $('.ems-header__nav').css('background','');
+  }
+}
